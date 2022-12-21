9af6392607fa43d6241fd05f6748c69d0572a3f8
flew <100080184+flewa@users.noreply.github.com>
2022-12-21 15:08:40 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var config = {
     "Name":"flew",
-    "ProfileUrl":"https://cdn.discordapp.com/avatars/711621039787999232/978b8aa320ba2059f3006346cf46244b.webp?size=512",
+    "ProfileUrl":"https://icon-library.com/images/discord-icon-white/discord-icon-white-3.jpg",
     "ProfileIcons":[
         ["github","https://github.com/flewa"],
         ["discord","https://discord.gg/w5ZuHPg3zz"]
